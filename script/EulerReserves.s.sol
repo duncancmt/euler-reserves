733d33d331202da4d4aa0dbe35d5d0525afc5b8c
Duncan Townsend <git@duncancmt.com>
2023-04-01 15:22:35 -0400

    Comments

--- script/EulerReserves.s.sol
+++ script/EulerReserves.s.sol
@@ -167,12 +167,27 @@ contract EulerReserves is Script {
     string memory outputKey = "outputKey";
     string memory outputJson;
     for (uint256 i; i < underlyings.length; i++) {
+      // get Euler reserve
       address underlying = underlyings[i];
       uint256 reserve = _MARKETS.underlyingToEToken(underlying).reserveBalanceUnderlying();
 
+      // structure output as:
+      //   "0xTokenAddress": {
+      //     decimals?: number,
+      //     name?: string,
+      //     reserve: number,
+      //     symbol?: string
+      //   },
+      //   ...
+      // underlyingKey is used both as the identifier for the temporary object
+      // and as the key that it's stored at in the outputKey object
       string memory underlyingKey = vm.toString(underlying);
       string memory underlyingJson = vm.serializeUint(underlyingKey, "reserve", reserve);
 
+      // Because try ... catch in Solidity is a piece of shit, we have to do our
+      // own manual checking of the ABIEncoding to avoid spurious
+      // reverts. Strictly speaking, this is slightly too lax in the checking,
+      // but in practice it works well enough.
       (bool success, bytes memory returnData) = underlying.staticcall(abi.encodeCall(IERC20Meta(underlying).name, ()));
       if (success && returnData.length > 64) {
         underlyingJson = vm.serializeString(underlyingKey, "name", abi.decode(returnData, (string)));
@@ -185,6 +200,9 @@ contract EulerReserves is Script {
       if (returnData.length >= 32) {
         underlyingJson = vm.serializeUint(underlyingKey, "decimals", abi.decode(returnData, (uint8)));
       }
+
+      // Store the temporary object in the global object at outputKey. The
+      // intermediate JSON is stored in outputJson before we write it to disk.
       outputJson = vm.serializeString(outputKey, underlyingKey, underlyingJson);
     }
     vm.writeJson(outputJson, "./output.json");
